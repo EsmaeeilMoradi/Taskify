9d5aa903e149a8a607f1fcc52e917c4d22324e8d
esmaeeilmoradi <esmaeeilmoradi700@gmail.com>
2025-04-08 10:24:23 +0400

    Completed Repository Interface, Repository Implementation, di module function for IoDispatcher. Data layer is done.

---
+++ app/src/main/java/com/esm/taskify/feature_todo/domain/repo/TodoListRepo.kt
@@ -0,0 +1,14 @@
+package com.esm.taskify.feature_todo.domain.repo
+
+import com.esm.taskify.feature_todo.domain.model.TodoItem
+
+
+interface TodoListRepo {
+    suspend fun getAllTodos(): List<TodoItem>
+    suspend fun getAllTodosFromLocalCache(): List<TodoItem>
+    suspend fun getAllTodosFromRemote()
+    suspend fun getSingleTodoItemById(id: Int): TodoItem?
+    suspend fun addTodoItem(todo:TodoItem)
+    suspend fun updateTodoItem(todo: TodoItem)
+    suspend fun deleteTodoItem(todo: TodoItem)
+}
