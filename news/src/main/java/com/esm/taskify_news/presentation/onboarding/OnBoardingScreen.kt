cc85e9ee6eb695c32ef44884ee99b8b6997c47a1
esmaeeilmoradi <esmaeeilmoradi700@gmail.com>
2025-04-14 10:55:10 +0400

    [News] Created OnBoarding Pages and Screen, Added Resources & Solved build packaging resources  error.

---
+++ news/src/main/java/com/esm/taskify_news/presentation/onboarding/OnBoardingScreen.kt
@@ -0,0 +1,34 @@
+package com.esm.taskify_news.presentation.onboarding
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import com.esm.taskify_news.presentation.onboarding.components.OnBoardingPage
+
+@Composable
+fun OnBoardingScreen() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        val pagerState = rememberPagerState(initialPage = 0) {
+            pages.size
+        }
+        val buttonState = remember {
+            derivedStateOf {
+                when (pagerState.currentPage) {
+                    0 -> listOf("", "Next")
+                    1 -> listOf("Back", "Next")
+                    2 -> listOf("Back", "Get Started")
+                    else -> listOf("", "")
+                }
+            }
+        }
+
+        HorizontalPager(state = pagerState) { index ->
+            OnBoardingPage(page = pages[index])
+        }
+    }
+}
