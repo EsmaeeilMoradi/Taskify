c8713ea6e2cc429032f5e041c9676eafe7fded43
esmaeeilmoradi <esmaeeilmoradi700@gmail.com>
2025-04-21 10:59:57 +0400

    [News] Fixed: Show EmptyScreen when ArticlesList is empty.

--- news/src/main/java/com/esm/taskify_news/presentation/common/ArticlesList.kt
+++ news/src/main/java/com/esm/taskify_news/presentation/common/ArticlesList.kt
@@ -15,11 +15,35 @@ import com.esm.taskify_news.presentation.Dimens.ExtraSmallPadding2
 import com.esm.taskify_news.presentation.Dimens.MediumPadding1
 import com.esm.taskify_news.presentation.home.components.ArticleCard
 
-
 @Composable
 fun ArticlesList(
     modifier: Modifier = Modifier,
     articles: List<Article>,
+    onClick: (Article) -> Unit
+) {
+    if (articles.isEmpty()){
+        EmptyScreen()
+    }
+    LazyColumn(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(MediumPadding1),
+        contentPadding = PaddingValues(all = ExtraSmallPadding2)
+    ) {
+        items(
+            count = articles.size,
+        ) {
+            articles[it]?.let { article ->
+                ArticleCard(article = article, onClick = { onClick(article) })
+            }
+        }
+    }
+
+}
+
+@Composable
+fun ArticlesList(
+    modifier: Modifier = Modifier,
+    articles: LazyPagingItems<Article>,
     onClick: (Article) -> Unit
 ) {
 
@@ -36,7 +60,7 @@ fun ArticlesList(
                 count = articles.itemCount,
             ) {
                 articles[it]?.let { article ->
-                    ArticleCard(article = article, onClick = {onClick(article)})
+                    ArticleCard(article = article, onClick = { onClick(article) })
                 }
             }
         }
@@ -72,8 +96,8 @@ fun handlePagingResult(articles: LazyPagingItems<Article>): Boolean {
 
 @Composable
 fun ShimmerEffect() {
-    Column(verticalArrangement = Arrangement.spacedBy(MediumPadding1)){
-        repeat(10){
+    Column(verticalArrangement = Arrangement.spacedBy(MediumPadding1)) {
+        repeat(10) {
             ArticleCardShimmerEffect(
                 modifier = Modifier.padding(horizontal = MediumPadding1)
             )
